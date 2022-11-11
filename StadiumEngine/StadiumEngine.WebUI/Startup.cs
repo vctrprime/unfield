cc11f96fbbbae9568f8f1e84aef18b9200f5ed47
kovalchuk <vctr.prime@gmail.com>
2022-11-11 23:36:26 +0300

    fix dep

--- StadiumEngine/StadiumEngine.WebUI/Startup.cs
+++ StadiumEngine/StadiumEngine.WebUI/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,6 +25,8 @@ namespace StadiumEngine.WebUI
             services.RegisterModules();
             
             services.AddControllersWithViews();
+            
+            services.Configure<KestrelServerOptions>(Configuration.GetSection("Kestrel"));
 
             // In production, the React files will be served from this directory
             services.AddSpaStaticFiles(configuration => { configuration.RootPath = "ClientApp/build"; });
@@ -38,16 +41,15 @@ namespace StadiumEngine.WebUI
             }
             else
             {
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseHttpsRedirection();
             }
-
-            app.UseForwardedHeaders(new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-            });
             
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
