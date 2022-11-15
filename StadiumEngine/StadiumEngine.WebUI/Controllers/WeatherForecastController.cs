7054b7c790a495119a0b0dc2e7fed1fb9773f80f
kovalchuk <vctr.prime@gmail.com>
2022-11-15 17:54:21 +0300

    logging

--- StadiumEngine/StadiumEngine.WebUI/Controllers/WeatherForecastController.cs
+++ StadiumEngine/StadiumEngine.WebUI/Controllers/WeatherForecastController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using StadiumEngine.DataAccess.Repositories.Abstract;
 using StadiumEngine.Entities;
 
@@ -12,10 +13,12 @@ namespace StadiumEngine.WebUI.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly ITestRepository _repository;
+        private readonly ILogger _logger;
 
-        public WeatherForecastController(ITestRepository repository)
+        public WeatherForecastController(ITestRepository repository, ILogger<WeatherForecastController> logger)
         {
             _repository = repository;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -23,6 +26,28 @@ namespace StadiumEngine.WebUI.Controllers
         {
             try
             {
+                _logger.LogInformation("terssdfs");
+                var result = await _repository.Get();
+                return result;
+            }
+            catch (Exception e)
+            {
+                return new List<Class1>
+                {
+                    new Class1
+                    {
+                        Name = e.Message + ";" + Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")+ ";" + Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+                    }
+                };
+            }
+        }
+        
+        [HttpGet("error")]
+        public async Task<List<Class1>> GetError()
+        {
+            try
+            {
+                _logger.LogError(new Exception("test"), "Тест");
                 var result = await _repository.Get();
                 return result;
             }
--- StadiumEngine/StadiumEngine.WebUI/Program.cs
+++ StadiumEngine/StadiumEngine.WebUI/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace StadiumEngine.WebUI
 {
@@ -13,11 +15,21 @@ namespace StadiumEngine.WebUI
     {
         public static void Main(string[] args)
         {
+            Serilog.Debugging.SelfLog.Enable(msg => Console.WriteLine($"Logging Process Error: {msg}"));
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console()
+                .WriteTo.PostgreSQL(connectionString: Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"),
+                    tableName: "PUBLIC.LOG_ERRORS",
+                    needAutoCreateTable: true,
+                    restrictedToMinimumLevel: LogEventLevel.Error)
+                .CreateLogger();
+            
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+            Host.CreateDefaultBuilder(args).UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
