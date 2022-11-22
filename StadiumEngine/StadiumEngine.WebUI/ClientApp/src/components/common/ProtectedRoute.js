efc9b950271188cf6149031c7f024af86ea840f8
kovalchuk <vctr.prime@gmail.com>
2022-11-22 20:45:24 +0300

    fix warning

--- StadiumEngine/StadiumEngine.WebUI/ClientApp/src/components/common/ProtectedRoute.js
+++ StadiumEngine/StadiumEngine.WebUI/ClientApp/src/components/common/ProtectedRoute.js
@@ -1,9 +1,8 @@
 import React from "react";
-import { Navigate, Route } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 function ProtectedRoute({ component: Component, ...restOfProps }) {
     const isAuthenticated = localStorage.getItem("isAuthenticated");
-    console.log("this", isAuthenticated);
     
     return isAuthenticated ? <Component {...restOfProps}/> : <Navigate to="/lk/sign-in" />
     
