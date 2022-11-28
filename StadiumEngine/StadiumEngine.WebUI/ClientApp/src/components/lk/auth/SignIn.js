066764d77a248f6c73e85ffd0b64683c77388291
kovalchuk <vctr.prime@gmail.com>
2022-11-28 22:07:03 +0300

    sign in form style

--- StadiumEngine/StadiumEngine.WebUI/ClientApp/src/components/lk/auth/SignIn.js
+++ StadiumEngine/StadiumEngine.WebUI/ClientApp/src/components/lk/auth/SignIn.js
@@ -37,7 +37,7 @@ export const SignIn = () => {
 
             
             
-            <h1>Signin User</h1>
+            
             <form
                 style={{
                     display: "flex",
@@ -47,7 +47,7 @@ export const SignIn = () => {
                 }}
             >
                 <div className="form-group">
-                    <label>Username</label>
+                    <label className="form-control-label">Логин</label>
                     <input
                         className="form-control"
                         type="text"
@@ -56,22 +56,32 @@ export const SignIn = () => {
                 </div>
 
                 <div className="form-group">
-                    <label>Password</label>
+                    <label className="form-control-label">Пароль</label>
                     <input
                         className="form-control"
                         type="password"
                         name="password"
                     />
+
+
+                    <button
+                        type="submit"
+                        onClick={handleSubmit}
+                    >
+                        Войти
+                    </button>
                 </div>
-                <button
-                    type="submit"
-                    className="btn btn-primary"
-                    onClick={handleSubmit}
-                >
-                    Submit
-                </button>
-                <NavLink className="btn btn-default portal-button" to="/">На портал</NavLink>
+                
+               
             </form>
+
+            <NavLink className="portal-button" to="/">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                     className="bi bi-house" viewBox="0 0 16 16">
+                    <path
+                        d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
+                </svg>
+            </NavLink>
            
         </div>
     );
