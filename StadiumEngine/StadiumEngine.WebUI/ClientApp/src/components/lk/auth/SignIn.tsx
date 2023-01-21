b15cf148bcdc3aa1da65e501027d4e23f4e4b86c
kovalchuk <vctr.prime@gmail.com>
2023-01-22 02:40:55 +0300

    sign in

--- StadiumEngine/StadiumEngine.WebUI/ClientApp/src/components/lk/auth/SignIn.tsx
+++ StadiumEngine/StadiumEngine.WebUI/ClientApp/src/components/lk/auth/SignIn.tsx
@@ -45,7 +45,7 @@ export const SignIn = () => {
 
     return (
         <div className="sign-in-container" style={{
-            background: `linear-gradient( rgba(255, 255, 255, 0.8), rgba(255, 255, 255, 0.8) ), no-repeat center / cover url(${cells})`
+            background: `linear-gradient( rgba(255, 255, 255, 0.9), rgba(255, 255, 255, 0.9) ), no-repeat center / cover url(${cells})`
         }}>
             <div className="color-block bottom-color-block" />
             <div className="color-block top-color-block" />
