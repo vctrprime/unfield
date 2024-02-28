af344615a731d3d61d33b9f72f7604ac994be760
kovalchuk <vctr.prime@gmail.com>
2024-02-28 20:19:15 +0300

    почистил контекст

--- StadiumEngine/StadiumEngine.Repositories/Infrastructure/Contexts/MainDbContext.cs
+++ StadiumEngine/StadiumEngine.Repositories/Infrastructure/Contexts/MainDbContext.cs
@@ -2,7 +2,6 @@ using Microsoft.EntityFrameworkCore;
 using StadiumEngine.Domain.Entities.Notifications;
 using StadiumEngine.Domain.Entities.Rates;
 using StadiumEngine.Domain.Entities.Settings;
-using StadiumEngine.Repositories.Infrastructure.Configurations;
 using StadiumEngine.Repositories.Infrastructure.Configurations.Accounts;
 using StadiumEngine.Repositories.Infrastructure.Configurations.Bookings;
 using StadiumEngine.Repositories.Infrastructure.Configurations.Geo;
@@ -22,21 +21,6 @@ internal class MainDbContext : DbContext
 
     protected override void OnModelCreating( ModelBuilder modelBuilder )
     {
-        //modelBuilder.Ignore<BaseEntity>();
-        //modelBuilder.Ignore<BaseUserEntity>();
-
-        /*foreach ( IMutableEntityType et in modelBuilder.Model.GetEntityTypes() )
-        {
-            if ( !et.ClrType.IsSubclassOf( typeof( BaseEntity ) ) )
-            {
-                continue;
-            }
-
-            //et.FindProperty( "DateCreated" )!.SetDefaultValueSql( "now()" );
-            //et.FindProperty( "DateCreated" )!.ValueGenerated =
-               // ValueGenerated.OnAdd;
-        }*/
-
         #region accounts
         
         modelBuilder.ApplyConfiguration( new LegalConfiguration() );
@@ -115,74 +99,4 @@ internal class MainDbContext : DbContext
         
         base.OnModelCreating( modelBuilder );
     }
-
-    #region geo
-
-    //public DbSet<Country> Countries { get; set; }
-    //public DbSet<Region> Regions { get; set; }
-    //public DbSet<City> Cities { get; set; }
-
-    #endregion
-
-    #region accounts
-
-    //public DbSet<Legal> Legals { get; set; }
-    //public DbSet<PermissionGroup> PermissionGroups { get; set; }
-    //public DbSet<Role> Roles { get; set; }
-    //public DbSet<User> Users { get; set; }
-    //public DbSet<Permission> Permissions { get; set; }
-    //public DbSet<RolePermission> RolePermissions { get; set; }
-    //public DbSet<UserStadium> UserStadiums { get; set; }
-    //public DbSet<Stadium> Stadiums { get; set; }
-
-    #endregion
-
-    #region offers
-
-    //public DbSet<LockerRoom> LockerRooms { get; set; }
-    //public DbSet<Field> Fields { get; set; }
-    //public DbSet<Inventory> Inventories { get; set; }
-    //public DbSet<OffersImage> OffersImages { get; set; }
-    //public DbSet<OffersSportKind> SportKinds { get; set; }
-
-    #endregion
-
-    #region rates
-
-    //public DbSet<Tariff> Tariffs { get; set; }
-    //public DbSet<DayInterval> DayIntervals { get; set; }
-    //public DbSet<TariffDayInterval> TariffDayIntervals { get; set; }
-    //public DbSet<Price> Prices { get; set; }
-    //public DbSet<PriceGroup> PriceGroups { get; set; }
-    //public DbSet<PriceGroup> PromoCodes { get; set; }
-
-    #endregion
-
-    #region settings
-
-    //public DbSet<MainSettings> MainSettings { get; set; }
-    //public DbSet<Break> Breaks { get; set; }
-    //public DbSet<BreakField> BreakFields { get; set; }
-
-    #endregion
-
-    #region booking
-    
-    //public DbSet<Booking> Bookings { get; set; }
-    //public DbSet<BookingCost> BookingCosts { get; set; }
-    //public DbSet<BookingCustomer> BookingCustomers { get; set; }
-    //public DbSet<BookingInventory> BookingInventories { get; set; }
-    //public DbSet<BookingLockerRoom> BookingLockerRooms { get; set; }
-    //public DbSet<BookingPromo> BookingPromos { get; set; }
-    //public DbSet<BookingWeeklyExcludeDay> BookingWeeklyExcludeDays { get; set; }
-
-    #endregion
-
-    #region notifications
-    
-    //public DbSet<UIMessage> UIMessages { get; set; }
-    //public DbSet<UIMessageText> UIMessageTexts { get; set; }
-    //public DbSet<UIMessageLastRead> UIMessageLastReads { get; set; }
-    
-    #endregion
 }
